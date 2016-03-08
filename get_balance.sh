9225b301500f8c35bba09ad52f518da2c76bcd36
Alejandro Rodriguez <arodriguez@onapsis.com>
2016-03-07 22:53:34 -0300

    Nuevo script para obtener balance en base a la cantidad de cuotapartes y el último valor conocido de estas.

---
+++ get_balance.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 2 ]; then
+    echo Uso: $0 archivo_data cantidad_cuotapartes
+    echo Ejemplo: $0 data.csv 1500
+    exit 1
+fi
+
+
+last_value=`tail -1 $1 | cut -d" " -f2 | sed s/,/./`
+echo "$last_value * $2" | bc
